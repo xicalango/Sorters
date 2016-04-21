ec4cb2340a90365c73f656eedb2d65db2ab52142
Alexander Weld <weldale@googlemail.com>
2016-04-21 11:42:14 +0100

    Added comments

--- src/com/lsm/ListSortMerger.java
+++ src/com/lsm/ListSortMerger.java
@@ -6,6 +6,7 @@ public interface ListSortMerger {
 
     static int[] createResultArray(InsertPosition insertPosition, int[] left, int[] right) {
         if (insertPosition == InsertPosition.BACK) {
+            // result array = left + right
             int[] result = Arrays.copyOf(left, left.length + right.length);
 
             System.arraycopy(right, 0, result, left.length, right.length);
@@ -13,11 +14,13 @@ public interface ListSortMerger {
         }
 
         if (insertPosition == InsertPosition.FRONT) {
+            // result array = right + left
             int[] result = Arrays.copyOf(right, left.length + right.length);
             System.arraycopy(left, 0, result, right.length, left.length);
             return result;
         }
 
+        // result array = left/2 + right + left/2
         assert insertPosition == InsertPosition.MIDDLE : insertPosition;
 
         int[] result = new int[left.length + right.length];
